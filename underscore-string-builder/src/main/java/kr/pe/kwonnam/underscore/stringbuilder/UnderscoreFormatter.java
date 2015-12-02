a7a54064bc66522940c10507bd59c403e25096bf
KwonNam Son <kwon37xi@gmail.com>
2015-12-02 23:34:33 +0900

    testing generics

---
+++ underscore-string-builder/src/main/java/kr/pe/kwonnam/underscore/stringbuilder/UnderscoreFormatter.java
@@ -0,0 +1,8 @@
+package kr.pe.kwonnam.underscore.stringbuilder;
+
+/**
+ * Format the appended string with arguments.
+ */
+public interface UnderscoreFormatter {
+    String format(String str, Object... args);
+}
