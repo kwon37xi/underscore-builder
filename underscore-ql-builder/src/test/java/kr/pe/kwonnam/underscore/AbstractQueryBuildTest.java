5b9f5ad2e64e44485eb58dba45646e5dc327c839
KwonNam Son <kwon37xi@gmail.com>
2015-12-15 20:45:53 +0900

    move up zipCodes

--- underscore-ql-builder/src/test/java/kr/pe/kwonnam/underscore/AbstractQueryBuildTest.java
+++ underscore-ql-builder/src/test/java/kr/pe/kwonnam/underscore/AbstractQueryBuildTest.java
@@ -3,9 +3,13 @@ package kr.pe.kwonnam.underscore;
 import org.junit.Before;
 
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.List;
 
 public abstract class AbstractQueryBuildTest {
     protected User user;
+
+    protected List<String> zipCodes;
 
     @Before
     public void setUp() throws Exception {
@@ -15,6 +19,7 @@ public abstract class AbstractQueryBuildTest {
         user.setName("String Builder");
         user.setBirthday(new SimpleDateFormat("yyyy/MM/dd").parse("2015/12/11"));
         user.setEmail("someone@email.com");
-    }
 
+        zipCodes  = Arrays.asList("12345", "56789", "58391");
+    }
 }
